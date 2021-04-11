4bd0ef0a3ef8fdb800d6329b77d6485b6981643d
Tebid Quinsy <28558929+lagom-QB@users.noreply.github.com>
2021-04-11 17:32:05 +0200

    Complete covid_tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import * as stateAbbr from "datasets-us-states-abbr";
+import Chart from './Chart.js'
 
 function App() {
+
+  const [activeQuery, setActiveQuery] = useState('NY')
+  const [stateData, setStateData] = useState({})
+
+  useEffect(() => {
+    async function getData(){
+      const res = await fetch(
+        `https://covidtracking.com/api/states?state=${activeQuery}`
+      ); // covid api
+      const data = await res.json();
+      setStateData(data);
+    }
+    getData();
+  }, [activeQuery]);
+
+  console.log(stateData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{fontFamily: "monospace", width:"90vw", textAlign:"center", overflow: "hidden", margin: "40px", padding: "40px"}}>
+      <h1> COVID_19 Test Tracker</h1>
+      <p style={{textAlign: "right"}}>Inspired by Justin Juno</p>
+
+      <form>
+        <label>
+          Choose a state: <t/>
+          <select value={activeQuery} onChange={e => setActiveQuery(e.target.value)}>
+            {Object.values(stateAbbr).map(abbr => {
+              return (
+                <option key={abbr} value={abbr}>
+                  {abbr}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+      </form>
+
+      <Chart data={stateData} />
     </div>
   );
 }
